fa1b8b97707c86a07b7fe20b094b4d37ea19d90c
akira-sasaki <gclue.akira@gmail.com>
2017-04-28 10:06:46 +0900

    Update RobotMouse.py

--- MotorShield/RobotMouse.py
+++ MotorShield/RobotMouse.py
@@ -59,7 +59,7 @@ class RobotMouse(threading.Thread):
     def map(self, x, in_min, in_max, out_min, out_max):
         return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
     
-    def right_back(self, speed):
+    def right_forward(self, speed):
         if speed < 0:
             print "value is under 0,  must define 1-100 as speed."
             return
@@ -72,7 +72,7 @@ class RobotMouse(threading.Thread):
         bus.write_i2c_block_data(self.address_right,CONTROL,[sval]) #生成したデータを送信
     
     # speedは0-100で指定
-    def right_forward(self, speed):
+    def right_back(self, speed):
         if speed < 0:
             print "value is under 0,  must define 1-100 as speed."
             return
@@ -87,7 +87,7 @@ class RobotMouse(threading.Thread):
     def right_stop(self):
         bus.write_i2c_block_data(self.address_right,CONTROL,[STOP]) #モータへの電力の供給を停止(惰性で動き続ける)
         
-    def left_forward(self, speed):
+    def left_back(self, speed):
         if speed < 0:
             print "value is under 0,  must define 1-100 as speed."
             return
@@ -100,7 +100,7 @@ class RobotMouse(threading.Thread):
         bus.write_i2c_block_data(self.address_left,CONTROL,[sval]) #生成したデータを送信
     
     # speedは0-100で指定
-    def left_back(self, speed):
+    def left_forward(self, speed):
         if speed < 0:
             print "value is under 0,  must define 1-100 as speed."
             return
